00266faade653cea36d4817a9fe1aa8f39f46b33
Mihucza Bence <mihben@gmail.com>
2022-09-26 15:07:24 +0200

    Set cors policy on proper way

--- src/RuneGlossary/Program.cs
+++ src/RuneGlossary/Program.cs
@@ -27,15 +27,21 @@ builder.AddCQS(builder =>
     builder.AddRequestRouter(request => "resurrected", builder => builder.AddGenericHttpSender("resurrected", (options, configuration) => configuration.Bind("Services:Resurrected", options)));
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("develop", builder =>
+    {
+        builder.WithHeaders("request-type");
+        builder.SetIsOriginAllowed(origin => origin.Equals("http://localhost:5000"));
+    });
+});
+
+
 var app = builder.Build();
 
 app.UseDefaultExceptionHandler();
 
-app.UseCors(builder =>
-{
-    builder.AllowAnyOrigin();
-    builder.AllowAnyHeader();
-});
+app.UseCors("develop");
 
 app.UseAuthentication();
 app.UseAuthorization();
