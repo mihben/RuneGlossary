b21828cd330b1f9eecdcdac61a5b73afec27a5f2
Bence Mihucza <mihben@gmail.com>
2022-10-03 22:11:01 +0200

    Implemented runes tooltip

--- src/RuneGlossary.Application/Performers/Queries/GetRunesQueryPerformer.cs
+++ src/RuneGlossary.Application/Performers/Queries/GetRunesQueryPerformer.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.Extensions.Logging;
+using RuneGlossary.Api;
 using STrain;
 using GetRunesQuery = RuneGlossary.Api.GetRunesQuery;
 
 namespace RuneGlossary.Application.Performers.Queries
 {
-    public class GetRunesQueryPerformer : IQueryPerformer<GetRunesQuery, IEnumerable<GetRunesQuery.Rune>>
+    public class GetRunesQueryPerformer : IQueryPerformer<GetRunesQuery, IEnumerable<Rune>>
     {
         private readonly IRequestSender _sender;
         private readonly ILogger<GetRunesQueryPerformer> _logger;
@@ -15,11 +16,11 @@ namespace RuneGlossary.Application.Performers.Queries
             _logger = logger;
         }
 
-        public async Task<IEnumerable<GetRunesQuery.Rune>> PerformAsync(GetRunesQuery query, CancellationToken cancellationToken)
+        public async Task<IEnumerable<Rune>> PerformAsync(GetRunesQuery query, CancellationToken cancellationToken)
         {
             _logger.LogInformation("Querying runes");
             return (await _sender.GetAsync<Resurrected.Api.GetRunesQuery, IEnumerable<Resurrected.Api.GetRunesQuery.Rune>>(new Resurrected.Api.GetRunesQuery(), cancellationToken))?
-                        .Select(r => new GetRunesQuery.Rune(r.Id, r.Name, r.Level, r.InHelmet, r.InBodyArmor, r.InShield, r.InWeapon)) ?? new List<GetRunesQuery.Rune>();
+                        .Select(r => new Rune(r.Id, r.Name, r.Level, r.InHelmet, r.InBodyArmor, r.InShield, r.InWeapon)) ?? Enumerable.Empty<Rune>();
         }
     }
 }
