107de3819ff9127c6ef5f337c70a672e6c2e73f3
Bence Mihucza <mihben@gmail.com>
2022-10-04 13:31:49 +0200

    Display class and character information

--- src/RuneGlossary.Client.WASM/Models/Character.cs
+++ src/RuneGlossary.Client.WASM/Models/Character.cs
@@ -7,12 +7,14 @@ namespace RuneGlossary.Client.WASM.Models
         public int Id { get; }
         public Class Class { get; }
         public string Name { get; }
+        public int Level { get; set; }
 
-        public Character(int id, Class @class, string name)
+        public Character(int id, Class @class, string name, int level)
         {
             Id = id;
             Class = @class;
             Name = name;
+            Level = level;
         }
     }
 }
