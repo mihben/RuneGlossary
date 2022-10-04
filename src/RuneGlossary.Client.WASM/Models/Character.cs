c40eed8da44a11788499b7eece74e43dcecb08ba
Mihucza Bence <mihben@gmail.com>
2022-10-04 09:59:47 +0200

    [WIP] - Implemented character data

---
+++ src/RuneGlossary.Client.WASM/Models/Character.cs
@@ -0,0 +1,18 @@
+﻿using RuneGlossary.Api.Responses;
+
+namespace RuneGlossary.Client.WASM.Models
+{
+    public class Character
+    {
+        public int Id { get; }
+        public Class Class { get; }
+        public string Name { get; }
+
+        public Character(int id, Class @class, string name)
+        {
+            Id = id;
+            Class = @class;
+            Name = name;
+        }
+    }
+}
